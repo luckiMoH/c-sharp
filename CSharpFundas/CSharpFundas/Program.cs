b4e079f2393e41bde5f2922282f3b9253922f8c8
Mateusz Majewski <mat.maj1992@gmail.com>
2023-06-28 11:38:44 +0200

    first steps with C#, IDE configuration etc

---
+++ CSharpFundas/CSharpFundas/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+string name = "John";
+int num1 = 15;
+int num2 = 10;
+if (num1 < num2)
+{
+    Console.WriteLine(num1 + " jest mniejsza od " + num2);
+} else
+{
+    Console.WriteLine(num2 + " jest mniejsza od " + num1);
+}
+Debug.WriteLine("Hello, World!");
+//namespace CSharpFundas
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World :) !");
+//            Console.WriteLine("Hello World :) !2");
+//        }
+//    }
+//}
